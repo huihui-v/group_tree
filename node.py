19dd15bc019116300801bb804516eede04b8a537
Jianfei2333 <xjf999999@hotmail.com>
2017-05-25 00:19:55 +0800

    v1.0 实现客户端插入组播树功能

--- node.py
+++ node.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import json
+import random
 import threading
 
 f = open("config.json", 'r+b')
@@ -75,7 +76,7 @@ def handle_msg_for_RP(s):
         if recv_info['status'] == 'REQ':
             if recv_info['body'] == 'new_connection':
                 ack = pack('ACK', 'req_connection_recved', recv_info['targetip'], recv_info['targetport'], addr[0], addr[1])
-                s.sendall(ack)
+                conn.sendall(ack)
                 if len(son_cons) < MAX_DEGREE:
                     new_conn = connect_to_new(recv_info['sourceip'], recv_info['sourceport'])
                     son_cons.append(new_conn);
@@ -120,8 +121,10 @@ def handle_connection(s):
         return;
     else:
         self_addr = s.getsockname()
-        create_connection_with_father_node(port)
-        package = pack("REQ", "new_connection", self_addr[0], port, host, port)
+        bind_port = random.randint(50000, 60000)
+        thread = threading.Thread(target=create_connection_with_father_node, args=(bind_port,))
+        thread.start()
+        package = pack("REQ", "new_connection", self_addr[0], bind_port, host, port)
         s.sendall(package)
         recv_info = json.loads(s.recv(1024))
         if recv_info['status'] == 'ACK' and recv_info['body'] == 'req_connection_recved':
@@ -142,10 +145,9 @@ def create_connection_with_father_node(con_port):
     global CON_STATUS
     s = wait_for_call_of_father_node(con_port)
     while CON_STATUS != 1:
-        conn, addr = s.accpet()
+        conn, addr = s.accept()
         CON_STATUS = 1
-    thread = threading.Thread(target=handle_msg_for_normal_node, args=(conn, addr,))
-    thread.start()
+    handle_msg_for_normal_node(conn)
     return;
 
 
